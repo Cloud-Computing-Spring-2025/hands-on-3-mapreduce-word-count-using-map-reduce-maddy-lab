7656566e0d567aee1394db5be6563e9034504889
Madhan Mannem <69673888+maddy-lab@users.noreply.github.com>
2025-02-11 19:12:03 +0000

    final commit

--- src/main/java/com/example/WordReducer.java
+++ src/main/java/com/example/WordReducer.java
@@ -1,5 +1,33 @@
 package com.example;
 
-public class WordReducer{
-    
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.*;
+
+public class WordReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    private Map<Text, Integer> wordCountMap = new HashMap<>();
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int sum = 0;
+        for (IntWritable val : values) {
+            sum += val.get();
+        }
+        wordCountMap.put(new Text(key), sum);
+    }
+
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        
+        List<Map.Entry<Text, Integer>> sortedList = new ArrayList<>(wordCountMap.entrySet());
+        sortedList.sort((a, b) -> b.getValue().compareTo(a.getValue()));
+
+        
+        for (Map.Entry<Text, Integer> entry : sortedList) {
+            context.write(entry.getKey(), new IntWritable(entry.getValue()));
+        }
+    }
 }
